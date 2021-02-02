6f6b8bcdc274a57a1fd0af32c514af064c069740
ossaris56 <ossaris56@gmail.com>
2021-02-03 02:52:56 +0800

    added playing music

--- DSA_Assignment/DSA_Assignment.cpp
+++ DSA_Assignment/DSA_Assignment.cpp
@@ -1,20 +1,37 @@
-﻿// DSA_Assignment.cpp : This file contains the 'main' function. Program execution begins and ends there.
+﻿// DSA_Assignment.cpp : This file contains the 'main' function. Program execution begins and ends
+// there.
 //
 
 #include <iostream>
+#include <filesystem>
+#include <Windows.h>
+#include <mmsystem.h>
+
+namespace fs = std::filesystem;
 
 int main()
 {
-  std::cout << "Hello World!\n";
+  int option = -1;
+
+  while (option != 0)
+  {
+    std::cout << "Music Player" << std::endl;
+    std::cout << "==========================" << std::endl;
+    std::cout << "[1] View songs" << std::endl;
+    std::cout << "[2] Organize songs" << std::endl;
+    std::cout << "[0] Exit" << std::endl;
+    std::cin >> option;
+
+    if (option == 1)
+    {
+      int stopMusic = 0;
+      PlaySound(TEXT("../assets/piano.wav"), NULL, SND_FILENAME | SND_ASYNC);
+      std::cout << "Press 1 to stop : ";
+      std::cin >> stopMusic;
+      if (stopMusic == 1)
+      {
+        PlaySound(NULL, 0, 0);
+      }
+    }
+  }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
