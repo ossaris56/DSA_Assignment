f75e8922a865842c0a1a32f6e6bc36a3ff12190e
ossaris56 <ossaris56@gmail.com>
2021-02-07 06:58:42 +0800

    changed double linked list data type and function names

--- DSA_Assignment/DoublyLinkedList.h
+++ DSA_Assignment/DoublyLinkedList.h
@@ -1,29 +1,31 @@
 #pragma once
 #include <iostream>
-using namespace std;
+#include <string>
 
 struct node
 {
-  int data;    // Data
-  node* prev;  // A reference to the previous node
-  node* next;  // A reference to the next node
+  std::string data;  // Data
+  node* prev;        // A reference to the previous node
+  node* next;        // A reference to the next node
 };
 
-class Doubly_Linked_List
+class DoublyLinkedList
 {
   node* front;  // points to first node of list
   node* end;    // points to first las of list
  public:
-  Doubly_Linked_List()
+  // TODO: move to cpp file
+  DoublyLinkedList()
   {
     front = NULL;
     end = NULL;
   }
-  void add_front(int);
-  void add_after(node*, int);
-  void add_before(node*, int);
-  void add_end(int);
-  void delete_node(node*);
-  void forward_traverse();
-  void backward_traverse();
+
+  void AddFront(std::string);
+  void AddAfter(node*, std::string);
+  void AddBefore(node*, std::string);
+  void AddEnd(std::string);
+  void DeleteNode(node*);
+  void ForwardTraverse();
+  void BackwardTraverse();
 };
