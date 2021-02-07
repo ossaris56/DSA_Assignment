00cd6546549347dfc44f4d36287348747d6f5174
ossaris56 <ossaris56@gmail.com>
2021-02-07 13:31:40 +0800

    fixed bug where program will not wait for user input when adding new playlist

--- DSA_Assignment/DSA_Assignment.cpp
+++ DSA_Assignment/DSA_Assignment.cpp
@@ -21,7 +21,6 @@ void PlayingSongMenu(DoublyLinkedList songQueue);
 void AllPlaylistsMenu(Vector<Playlist*>* playlists);
 void PlaylistMenu(Playlist* playlist);
 Vector<TreeNode*> GetRootMusicDirectory();
-DoublyLinkedList playlistQueue;
 
 namespace SongVariables
 {
@@ -100,9 +99,11 @@ void AllPlaylistsMenu(Vector<Playlist*>* playlists)
   if (option == (playlists->Size() + 1))
   {
     std::cout << std::endl;
-    std::string playlistName;
     std::cout << "Enter name of the new playlist : ";
-    std::cin >> playlistName;
+    std::string playlistName;
+    // Flush cin input buffer so that the program will wait for user input
+    std::cin.ignore();
+    std::getline(std::cin, playlistName);
     Playlist* newPlaylist = new Playlist();
     newPlaylist->name = playlistName;
     newPlaylist->songs = new DoublyLinkedList();
